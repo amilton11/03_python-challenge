eb5cfe0d1a0c8c201aa5ad1ca868843f131097fd
Abigail <amourpropre@icloud.com>
2021-02-08 15:23:24 -0600

    progress

--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -4,50 +4,72 @@
 #The total number of votes each candidate won
 #The winner of the election based on popular vote.
 
+# import dependancies
 import os
 import csv
 
-candidates = {}
+# create variables to capture information
 
-election_csv = os.path.join('.','Resources','election_data.csv')
-with open(election_csv, 'r') as csvfile:
+# a counter
+total_votes = 0
 
+# a list
+candidates = []
+
+# a dictionary
+percentage_vote_by_candidate = {}
+
+# a dictionary
+votes_per_candidate = {}
+
+# open and read the file
+with open('Resources/PyPoll_Resources_data.csv', newline = '') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    header = next(csvreader)
 
+    # skip the header
+    next(csvreader, None)
+
+    # create a for loop
     for row in csvreader:
-        if row[2] in candidates.keys():
-            candidates[row[2]]+=1
-        else:
-            candidates[row[2]] = 1
-
-        total = candidates.values()
-        total_votes = sum(total)
-
-            
-        list_candidates = candidates.keys()
-            
-        votes_per = [f'{(x/total_votes)*100:.3f}%' for x in candidates.values()]
-            
-
-        winner = list(candidates.keys())[list(candidates.values()).index(max(candidates.values()))]
-        winner
         
+        # count the number of total votes and increase by 1
+        total_votes += 1
 
+        # loop through csv file and gather candidates for the candidates list
+        if row[2] in candidates and row[2] not in 'Candidates':
+            votes_per_candidate[row[2]] = votes_per_candidate[row[2]] + 1
 
-print("Election results")
+        else:
+            candidates.append(row[2])
+            votes_per_candidate[row[2]] = 1
+        
+# % calculation
+for key, value in votes_per_candidate.items():
+    percentage_vote_by_candidate[key] = str(round((value/total_votes)*100, 3)) + "% ("+str(value)+ ")"
 
+# find the winner
+winner = max(votes_per_candidate.keys(), key=(lambda k: votes_per_candidate[k]))
+winner
+
+ # print results      
+print("Election Results")
 print("--------------------------------")
-
-print(f" Total votes: {int(total_votes)}")
-
+print(f"Total votes: {total_votes}")
 print("---------------------------------")
-i = 0
-for candidate, vote in candidates.items():
-    print(f'{candidate}, {vote} , {votes_per[i]}') 
-    i+=1
+print(f"Percentage: {percentage_vote_by_candidate}")
+print("------------------------------")
+print(f"Winner: {winner}")
 print("------------------------------")
 
-print(f" Winner: {winner}")
+# output txt file
+election_results = os.path.join("election_results.txt")
+with open(election_results, "w") as outfile:  
 
-print("------------------------------")
+    outfile.write("Election Results\n")
+    outfile.write("--------------------------------\n")
+    outfile.write(f"Total votes: {total_votes}\n")
+    outfile.write("---------------------------------\n")
+    outfile.write(f"Percentage: {percentage_vote_by_candidate}\n")
+    outfile.write("------------------------------\n")
+    outfile.write(f"Winner: {winner}\n")
+    outfile.write("------------------------------\n")
