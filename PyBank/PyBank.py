f24d592d6db86a9c61b7a67fb0e9f1a5c8aede67
Abigail <amourpropre@icloud.com>
2021-02-08 18:22:02 -0600

    progress

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -5,7 +5,7 @@
 ##The greatest increase in profits (date and amount) over the entire period
 ##The greatest decrease in losses (date and amount) over the entire period
 
-# os is the file reference
+# os = operating system module
 import os
 
 # telling python the file type I am using 
@@ -13,62 +13,79 @@ import csv
 
 # assign the file I want to reference
 #mainPyBank = the scripts I am writing
-mainPyBank = os.path.join(".", "PyBank_Resources_data.csv")
+mainPyBank = os.path.join("Resources", "PyBank_Resources_data.csv")
 
 # assign the variables
-total_months = 0
-net_profit_loss = 0
-change_profit_loss = []
-change_date = []
-greatest_increase = []
-greatest_decrease = []
+profit = []
+monthly_changes = []
+date = []
 
-# opening the file in "read" "r" mode and simplify the file name reference
-with open(mainPyBank, "r") as csv_file:
+total_months = 0
+total_profit = 0
+total_change_profits = 0
+initial_profit = 0
+
+# opening the file 
+with open(mainPyBank, newline="") as csv_file:
+    
+    # establish the reader
     csv_reader = csv.reader(csv_file, delimiter = ",")
 
     # skip header
-    csv_header = next(csv_file)
+    csv_header = next(csv_reader)
 
     # Calculate total number of months included in the data set
     # Iterate through the rows in the stored file contents
     for row in csv_reader: 
             
-        ##The total number of months included in the dataset
-        #date.append(row 0)
-        #print(date)
-        total_months +=1
+        # count the total number of months included in the dataset
+        total_months = total_months + 1
     
+        # date needed for greatest increase and decrease
+        date.append(row[0])
 
-        ##The net total amount of "Profit/Losses" over the entire period
-        # Calculate net total amount of "Profit/Losses" over the entire period
-        # Iterate through the rows in the stored file contents
-        net_profit_loss += int(row[1])
+        # append profit information
+        profit.append(row[0])
+        total_profit = total_profit + int(row[1])
+
+        #Calculate the average change in profits from month to month. Then calulate the average change in profits
+        final_profit = int(row[1])
+        monthly_change_profits = final_profit - initial_profit
+
+        #Store these monthly changes in a list
+        monthly_changes.append(monthly_change_profits)
+        total_change_profits = total_change_profits + monthly_change_profits
+        initial_profit = final_profit
         
-        ##The average of the changes in "Profit/Losses" over the entire period
-        # Calculate average of the changes in "Profit/Losses" over the entire period
-        # Iterate through the profits in order to get the monthly change in profits
-        change_profit_loss.append(int(row[1]))
-        change_date.append(row[0])
+        # calculate the average change in profits
+        average_change_profits = (total_change_profits/total_months)
         
-        ##The greatest increase in profits (date and amount) over the entire period
-        # Calculate greatest increase in profits (date and amount) over the entire period
-        # Obtain the max and min of the the montly profit change list
-        greatest_increase = max(change_profit_loss)
-        greatest_decrease = min(change_profit_loss)
+        # find the max and min profit change
+        greatest_increase_profits = max(monthly_changes)
+        greatest_decrease_profits = min(monthly_changes)
+       
+        # find the dates the max and min profit change occured
+        increase_date = date[monthly_changes.index(greatest_increase_profits)]
+        decrease_date = date[monthly_changes.index(greatest_decrease_profits)]
 
-        ##The greatest decrease in losses (date and amount) over the entire period
-        # Calculate greatest decrease in losses (date and amount) over the entire period
-        # Correlate max and min to the proper month using month list and index from max and min
-        # We use the plus 1 at the end since month associated with change is the + 1 month or next month
-        #max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
-        #max_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1 
 
-ave_profit = net_profit_loss/total_months
-#Financial Analysis
-print(total_months)
-print(net_profit_loss)
-#print(change_profit_loss)
-print(ave_profit)
-print(greatest_increase)
-print(greatest_decrease)
+ # print results      
+print("Financial Analysis")
+print("--------------------------------")
+print(f"Total Months: " + str(total_months))
+print(f"Total Profits: " + "$" + str(total_profit))
+print(f"Average Change: " + "$" + str(int(average_change_profits)))
+print(f"Greatest Increase in Profits: " + str(increase_date) + " ($" + str(greatest_increase_profits) + ")")
+print(f"Greatest Decrease in Profits: " + str(decrease_date) + " ($" + str(greatest_decrease_profits) + ")")
+
+# output txt file
+finanacial_analysis = os.path.join("finanacial_analysis.txt")
+with open(finanacial_analysis, "w") as outfile:  
+
+    outfile.write("Financial Analysis\n")
+    outfile.write("--------------------------------\n")
+    outfile.write(f"Total Months: " + str(total_months)+ "\n")
+    outfile.write(f"Total Profits: " + "$" + str(total_profit))
+    outfile.write(f"Average Change: " + "$" + str(int(average_change_profits))+ "\n")
+    outfile.write(f"Greatest Increase in Profits: " + str(increase_date) + " ($" + str(greatest_increase_profits) + ")" + "\n")
+    outfile.write(f"Greatest Decrease in Profits: " + str(decrease_date) + " ($" + str(greatest_decrease_profits) + ")" + "\n") 
